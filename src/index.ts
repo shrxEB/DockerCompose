feb69af37e0333aa7f60bd990164ed325fa0b313
Shrayans Kumar Singh <shrayanskumarsingh@Shrayanss-MacBook-Pro.local>
2026-05-30 12:39:58 +0530

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import { PrismaClient } from "./generated/prisma/client.js";
+const prismaClient = new PrismaClient();
+const app = express()
+app.get("/", async (req,res) => {
+    const data = await prismaClient.user.findMany();
+    res.json({
+        data
+    })
+})
+app.post("/", async (req,res) => {
+    await prismaClient.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    })
+    res.json({
+        "message": "post endpoint"
+    })
+})
+app.listen(3000)
